b953a5a58c996d1c02c81a23997e2bc5eed8fb32
AntonDegt <anton11deg@gmail.com>
2024-07-21 19:10:51 +0300

    Добавьте файлы проекта.

---
+++ PestControll CRM/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PestControll_CRM
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public const string server_name = "";
+        
+        public string GetConnectionString (string server_name, string login, string password)
+        {
+            return $"{server_name}{login}{password}";
+        }
+
+    }
+}
